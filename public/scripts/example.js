401f5adf3c3db3142a6edde79eb80f49a5f7697c
sogo.ohta <sogo.ohta@gmail.com>
2014-12-09 13:00:02 +0900

    Fix

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -3,26 +3,6 @@
  */
 
 var converter = new Showdown.converter();
-
-var Member = React.createClass({
-  getInitialState: function() {
-    return {checked: this.props.checkallChecked};
-  },
-
-  handleChange: function(event) {
-    this.setState({checked: event.target.checked});
-  },
-
-  render: function() {
-    return (
-      <tr className="member">
-        <td><input type="checkbox" name={this.props.key} checked={this.state.checked} onChange={this.handleChange} /></td>
-        <td className="memberName">{this.props.name}</td>
-      </tr>
-    );
-  }
-});
-
 var MemberBox = React.createClass({
   getInitialState: function() {
     return {
@@ -42,6 +22,7 @@ var MemberBox = React.createClass({
     return (
       <div className="row">
         <div className="col-md-6 col-md-offset-3">
+          <h1>React</h1>
           <form role="form">
             <MemberList data={this.state.data} checkallChecked={this.state.checkallChecked} />
           </form>
@@ -51,52 +32,10 @@ var MemberBox = React.createClass({
   },
 });
 
-var MemberList = React.createClass({
-  getInitialState: function() {
-    var data = [];
-    this.props.data.forEach(function(d, i) {
-       d.checked = false;
-       data.push(d);
-    });
-    return {data: data, checkallChecked: this.props.checkallChecked };
-  },
-
-  render: function() {
-    var that = this;
-    var memberNodes = this.state.data.map(function(member, index) {
-      return (
-        <tr className="member" key={member.id}>
-          <td><input type="checkbox" data-index={index} checked={member.checked} onChange={that.handleChange} /></td>
-          <td className="memberName">{member.name}</td>
-        </tr>
-      );
-    });
-
-    return (
-      <div>
-        <table className="table">
-        <thead>
-          <tr>
-            <th><input type="checkbox" name="checkall" ref="checkall" checked={ this.state.checkallChecked } onChange={ this.handleAllChange } /></th>
-            <th>Name</th>
-          </tr>
-        </thead>
-        <tbody>{memberNodes}</tbody>
-        </table>
-        <div>
-          <button type="button" className="btn btn-default" onClick={ this.checkAll }>check all</button>
-          <button type="button" className="btn btn-default" onClick={ this.uncheckAll }>uncheck all</button>
-        </div>
-      </div>
-    );
-  },
-
+var CheckControlMixin = {
   handleAllChange: function(event) {
     var checked = event.target.checked;
-    var newData = this.state.data.map(function(d) {
-      return {id: d.id, name: d.name, checked: checked };
-    });
-    this.setState({checkallChecked: checked, data: newData});
+    this.checkControl(checked);
   },
 
   handleChange: function(event) {
@@ -128,17 +67,78 @@ var MemberList = React.createClass({
   },
 
   checkAll: function() {
-    var newData = this.state.data.map(function(d) {
-      return {id: d.id, name: d.name, checked: true };
-    });
-    this.setState({ checkallChecked: true, data: newData });
+    this.checkControl(true);
   },
 
   uncheckAll: function() {
+    this.checkControl(false);
+  },
+
+  checkControl: function(checked) {
     var newData = this.state.data.map(function(d) {
-      return {id: d.id, name: d.name, checked: false };
+      return {id: d.id, name: d.name, checked: checked };
     });
-    this.setState({ checkallChecked: false, data: newData });
+    this.setState({ checkallChecked: checked, data: newData });
+  }
+};
+
+var MemberList = React.createClass({
+  mixins: [CheckControlMixin],
+  getInitialState: function() {
+    var data = [];
+    this.props.data.forEach(function(d, i) {
+       d.checked = this.props.checkallChecked;
+       data.push(d);
+    }.bind(this));
+    return {data: data, checkallChecked: this.props.checkallChecked };
+  },
+
+  render: function() {
+    var memberNodes = this.state.data.map(function(member, index) {
+      return (
+        <tr className="member" key={member.id}>
+          <td><input type="checkbox" data-index={index} checked={member.checked} onChange={this.handleChange} /></td>
+          <td className="memberName">{member.name}</td>
+        </tr>
+      );
+    }.bind(this));
+
+    return (
+      <div>
+        <table className="table">
+        <thead>
+          <tr>
+            <th><input type="checkbox" name="checkall" ref="checkall" checked={ this.state.checkallChecked } onChange={ this.handleAllChange } /></th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>{memberNodes}</tbody>
+        </table>
+        <div>
+          <button type="button" className="btn btn-default" onClick={ this.checkAll }>check all</button>
+          <button type="button" className="btn btn-default" onClick={ this.uncheckAll }>uncheck all</button>
+        </div>
+      </div>
+    );
+  }
+});
+
+var Member = React.createClass({
+  getInitialState: function() {
+    return {checked: this.props.checkallChecked};
+  },
+
+  handleChange: function(event) {
+    this.setState({checked: event.target.checked});
+  },
+
+  render: function() {
+    return (
+      <tr className="member">
+        <td><input type="checkbox" name={this.props.key} checked={this.state.checked} onChange={this.handleChange} /></td>
+        <td className="memberName">{this.props.name}</td>
+      </tr>
+    );
   }
 });
 
